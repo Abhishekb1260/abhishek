8ac28662c1edb88867aa8e38dacc409ab6c182a2
Abhishekb1260 <86590264+Abhishekb1260@users.noreply.github.com>
2021-06-28 13:00:54 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,21 @@
+const countdown = () => {
+    const CountDate = new Date('September 01 , 2021 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = CountDate - now;
+    const second = 1000;
+    const min = second * 60;
+    const hour = min * 60;
+    const day = hour * 24;
+
+    const textday = Math.floor(gap/day);
+    const texthour = Math.floor((gap%day)/hour)
+    const textmin = Math.floor((gap%hour)/min)
+    const textsec = Math.floor((gap%min)/second)
+
+    document.querySelector(".days").innerHTML = textday
+    document.querySelector(".hours").innerHTML = texthour 
+    document.querySelector(".min").innerHTML = textmin
+    document.querySelector(".sec").innerHTML = textsec
+};
+
+setInterval(countdown , 1000)
